aff62c6a19e9955e0685a5b1623d820bb9e53d03
gaoheming <gao_hm@suixingpay.com>
2019-01-14 16:28:02 +0800

    index.js配置路由

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+}from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import './style.css'
+import Index from './pages/Index';
+import Login from './pages/Login';
+import About from "./pages/About";
+import logo from "./img/logo1.jpg";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default class All extends Component{
+    render(){
+        return (
+            <div>
+                <Router>
+                    <div className="wrap">
+                        <ul className="nav">
+                            <li><Link to="/Index" style={{color:'white',textDecoration:'none'}} >返回首页<span style={{color:'orange'}}>欢迎来到格林涂料</span></Link></li>
+                            <li><Link to="/Login" style={{color:'white',textDecoration:'none'}} >登陆/注册</Link></li>
+                        </ul>
+                        <div className="header">
+                            <div><img src={logo} alt="logo" /></div>
+                            <ul>
+                                <li><Link to="/Index" style={{color:'black',textDecoration:'none'}}>首页</Link></li>
+                                <li><Link to="/About" style={{color:'black',textDecoration:'none'}}>资讯</Link></li>
+                                <li><Link to="/About" style={{color:'black',textDecoration:'none'}}>生活</Link></li>
+                                <li><Link to="/About" style={{color:'black',textDecoration:'none'}}>产品</Link></li>
+                                <li><Link to="/About" style={{color:'black',textDecoration:'none'}}>装修建议</Link></li>
+                                <li><Link to="/About" style={{color:'black',textDecoration:'none'}}>公司简介</Link></li>
+                            </ul>
+                        </div>
+                        <Route path="/Login" component={Login} />
+                        <Route path="/Index" component={Index} />
+                        <Route path="/About" component={About} />
+                    </div>
+                </Router>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<All />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import '../style.css';
+
+export default class Index extends Component {
+    render() {
+        return (
+            <div>
+                <p>111111111</p>
+            </div>
+        );
+    }
+}
