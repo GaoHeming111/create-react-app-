bff8f1c5cfa2334a44f3124770cf97eec4eaeb93
gaoheming <gao_hm@suixingpay.com>
2019-01-14 16:56:13 +0800

    配置路由

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import '../style.css';
+import {
+    Link
+}from 'react-router-dom';
 
 export default class Index extends Component {
+
     render() {
         return (
             <div>
-                <p>111111111</p>
+                <Link to="/About">111111</Link>
             </div>
         );
     }
